84b5fae37bf8ffeeb3b1ab911a3eafa29cc57743
PhilMarek <Phil.Marek@rub.de>
2017-12-10 13:17:04 +0100

    minor refactoring

--- src/de/mpc/pqi/view/table/Table.java
+++ src/de/mpc/pqi/view/table/Table.java
@@ -25,36 +25,49 @@ public class Table {
 	private JScrollPane pane;
 
 	public JScrollPane initTable(PeptideModel peptideModel) {
+		this.table = new JBroTable();
 
-		List<Long> values = new ArrayList<>();
+		this.table.setBackground(Color.WHITE);
 
+		this.pane = this.table.getScrollPane();
+
+		
+		if (peptideModel != null) {
+			initData(peptideModel);
+		}
+		return this.pane;
+	}
+
+	private void initData(PeptideModel peptideModel) {
+		List<Double> values = new ArrayList<>();
+		
 		ModelField peptide = new ModelField(peptideModel.getName(), peptideModel.getName());
-
+	
 		ModelFieldGroup abundance = new ModelFieldGroup("Abundance", "Abundance");
-
+	
 		for (State state : peptideModel.getStates()) {
-
+	
 			ModelFieldGroup s = new ModelFieldGroup(UUID.randomUUID().toString(), state.getName());
-
+	
 			for (Run run : state.getRuns()) {
 				
 				values.add(run.getAbundance());
 				
 				ModelField r = new ModelField(UUID.randomUUID().toString(), run.getName());
-
+	
 				s.withChild(r);
 			}
 			abundance.withChild(s);
 		}
-
+	
 		IModelFieldGroup groups[] = new IModelFieldGroup[] { peptide, abundance };
-
+	
 		ModelField fields[] = ModelFieldGroup.getBottomFields(groups);
-
+	
 		ModelRow rows[] = new ModelRow[1];
-
+	
 		rows[0] = new ModelRow(fields.length);
-
+	
 		for (int i = 0; i < fields.length; i++) {
 			if (i == 0) {
 				rows[0].setValue(i, peptideModel.getName());
@@ -62,21 +75,15 @@ public class Table {
 				rows[0].setValue(i, values.get(i - 1));
 			}
 		}
-
+	
 		ModelData data = new ModelData(groups);
 		data.setRows(rows);
-
-		this.table = new JBroTable(data);
-
-		this.table.setBackground(Color.WHITE);
-
-		this.pane = this.table.getScrollPane();
-
-		return this.pane;
+	
+		this.table.setData(data);
 	}
-
+	
 	public void updateTable(PeptideModel peptideModel) {
-		List<Long> values = new ArrayList<>();
+		List<Double> values = new ArrayList<>();
 
 		for (State state : peptideModel.getStates()) {
 			for (Run run : state.getRuns()) {
@@ -84,29 +91,31 @@ public class Table {
 			}
 		}
 
-		ModelRow[] r = this.table.getData().getRows();
-
-		List<ModelRow> rowList = new ArrayList<ModelRow>(Arrays.asList(r));
-
-		ModelRow row = new ModelRow(this.table.getData().getFieldsCount());
-
-		for (int i = 0; i < this.table.getData().getFieldsCount(); i++) {
-			if (i == 0) {
-				row.setValue(i, peptideModel.getName());
-			} else {
-				row.setValue(i, values.get(i - 1));
+		if (this.table.getData() == null) initData(peptideModel);
+		else {
+			
+			ModelRow[] r = this.table.getData().getRows();
+	
+			List<ModelRow> rowList = new ArrayList<ModelRow>(Arrays.asList(r));
+	
+			ModelRow row = new ModelRow(this.table.getData().getFieldsCount());
+	
+			for (int i = 0; i < this.table.getData().getFieldsCount(); i++) {
+				if (i == 0) {
+					row.setValue(i, peptideModel.getName());
+				} else {
+					row.setValue(i, values.get(i - 1));
+				}
 			}
+	
+			rowList.add(row);
+	
+			ModelRow[] rows = new ModelRow[rowList.size()];
+			rows = rowList.toArray(rows);
+	
+			this.table.getData().setRows(rows);
 		}
-
-		rowList.add(row);
-
-		ModelRow[] rows = new ModelRow[rowList.size()];
-		rows = rowList.toArray(rows);
-
-		this.table.getData().setRows(rows);
-
 		this.table.revalidate();
-
 	}
 
 	public JBroTable getTable() {
