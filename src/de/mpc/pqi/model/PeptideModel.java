6d933ad870a70818ef9de42c832c1cf55c21c022
PhilMarek <Phil.Marek@rub.de>
2017-12-19 10:29:54 +0100

    added field usedForQuantification

--- src/de/mpc/pqi/model/PeptideModel.java
+++ src/de/mpc/pqi/model/PeptideModel.java
@@ -3,8 +3,8 @@ package de.mpc.pqi.model;
 import java.util.List;
 
 /**
- * Specialization of PQIModel. Store information for peptides, e.g. the peptide
- * name, abundances. Furthermore, a peptide can be deactivated for output.
+ * Specialization of PQIModel. Store information for peptides, e.g. the peptide name, abundances.
+ * Furthermore, a peptide can be deactivated for output.
  * 
  * @author Phil Marek
  *
@@ -14,12 +14,12 @@ public class PeptideModel extends PQIModel {
 		public static class Run {
 			private String name;
 			private double abundance;
-
+			
 			public Run(String name, double abundance) {
 				this.name = name;
 				this.abundance = abundance;
 			}
-
+			
 			public String getName() {
 				return name;
 			}
@@ -28,48 +28,42 @@ public class PeptideModel extends PQIModel {
 				return abundance;
 			}
 		}
-
+		
 		private String name;
 		private List<Run> runs;
-
+		
 		public State(String name, List<Run> runs) {
 			this.name = name;
 			this.runs = runs;
 		}
-
+		
 		public String getName() {
 			return name;
 		}
-
+		
 		public List<Run> getRuns() {
 			return runs;
 		}
 	}
-
+	
 	private String name;
+	/** The boolean indicating if this protein is used for quantification **/
+	private boolean usedForQuantification;
 	private boolean activated = true;
 	private List<State> states;
-	private boolean unique = false;
-
+	private boolean unique;
+	
 	public PeptideModel(String name, List<State> states) {
 		this.name = name;
-		this.states = states;
+		this.states  = states;
 	}
-
+	
 	public boolean isActivated() {
 		return activated;
 	}
 
 	public void setActivated(boolean activated) {
 		this.activated = activated;
-	}
-
-	public boolean isUnique() {
-		return this.unique;
-	}
-
-	public void setUnique(boolean unique) {
-		this.unique = unique;
 	}
 
 	@Override
@@ -79,5 +73,21 @@ public class PeptideModel extends PQIModel {
 
 	public List<State> getStates() {
 		return states;
+	}
+	
+	public boolean isUsedForQuantification() {
+		return usedForQuantification;
+	}
+
+	public void setUsedForQuantification(boolean usedForQuantification) {
+		this.usedForQuantification = usedForQuantification;
+	}
+
+	public boolean isUnique() {
+		return unique;
+	}
+
+	public void setUnique(boolean unique) {
+		this.unique = unique;
 	}
 }
