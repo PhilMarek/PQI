effa95af0223e8dca00adb3571455e64e5df416e
PhilMarek <Phil.Marek@rub.de>
2018-01-31 02:29:09 +0100

    refactoring

--- src/de/mpc/pqi/aggregation/AggregationView.java
+++ src/de/mpc/pqi/aggregation/AggregationView.java
@@ -13,87 +13,74 @@ import de.mpc.pqi.model.protein.ProteinModel;
 import de.mpc.pqi.view.GridBagHelper;
 
 public class AggregationView extends JPanel {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 2665924781251637075L;
 
 	private AggregationModel model;
 
 	private JLabel proteinNameLabel;
+	
 	private JLabel referenceLabel;
+	private JComboBox<String> referenceComboBox;
 
-	private JComboBox<String> firstComboBox;
-
-	private AggregationTabel table;
-
-	private GridBagHelper constraints = new GridBagHelper(new double[] { 0, 0.2 }, new double[] { 0, 0, 0.1});
-
-	private GridBagLayout layout;
+	private AggregationTable table;
 
 	public AggregationView() {
 		this.model = new AggregationModel();
 		initGui();
 		initLayout();
-		initControle();
+		initControl();
 	}
 
 	private void initGui() {
 		this.proteinNameLabel = new JLabel("Protein: ");
 		this.referenceLabel = new JLabel("Reference: ");
 
-		this.firstComboBox = new JComboBox<String>();
+		this.referenceComboBox = new JComboBox<String>();
 
-		this.table = new AggregationTabel();
+		this.table = new AggregationTable();
 
 	}
 
 	private void initLayout() {
-
-		this.layout = new GridBagLayout();
-		setLayout(this.layout);
+		GridBagHelper constraints = new GridBagHelper(new double[] { 0, 0.2 }, new double[] { 0, 0, 0.1});
+		setLayout(new GridBagLayout());
 
 		add(referenceLabel, constraints.getConstraints(0, 0));
-		add(firstComboBox, constraints.getConstraints(1, 0));
+		add(referenceComboBox, constraints.getConstraints(1, 0));
 
 		add(proteinNameLabel, constraints.getConstraints(2, 0));
-		add(table, constraints.getConstraints(0, 1, 3, 1));
+		add(table, constraints.getConstraints(0, 1, 4, 1));
 	}
-
-	public void update(ProteinModel proteinModel) {
-
-		firstComboBox.removeAllItems();
-
-		for (State state : proteinModel.getPeptides().get(0).getStates()) {
-			firstComboBox.addItem(state.getName());
-		}
-		firstComboBox.setSelectedIndex(0);
-
-		this.model.setSelectedState(firstComboBox.getSelectedItem().toString());
-
-		this.model.setProteinModel(proteinModel);
-
-		this.table.update(model.getTableModel());
-		this.proteinNameLabel.setText("Protein: " + proteinModel.getName());
-
-	}
-
-	private void initControle() {
-
-		firstComboBox.addItemListener(new ItemListener() {
-
+	
+	private void initControl() {
+		referenceComboBox.addItemListener(new ItemListener() {
 			@Override
 			public void itemStateChanged(ItemEvent e) {
 				if (e.getStateChange() == ItemEvent.SELECTED) {
-
-					if (firstComboBox.getSelectedItem() != null) {
-						model.setSelectedState(firstComboBox.getSelectedItem().toString());
+					if (referenceComboBox.getSelectedItem() != null) {
+						model.setSelectedState(referenceComboBox.getSelectedItem().toString());
 						table.update(model.getTableModel());
 					}
 				}
-
 			}
 		});
+	}
+
+	public void setModel(ProteinModel proteinModel) {
+		//TODO states are fixed in the names, thus keep them in combo box and keep the selection
+		referenceComboBox.removeAllItems();
+		for (State state : proteinModel.getPeptides().get(0).getStates()) {
+			referenceComboBox.addItem(state.getName());
+		}
+		if (referenceComboBox.getItemCount() != 0) {
+			referenceComboBox.setSelectedIndex(0);
+			model.setSelectedState(referenceComboBox.getSelectedItem().toString());
+		}
+		
+		model.setProteinModel(proteinModel);
+
+		table.update(model.getTableModel());
+		
+		proteinNameLabel.setText("Protein: " + proteinModel.getName() + " " + proteinModel.getDescription());
 	}
 }
